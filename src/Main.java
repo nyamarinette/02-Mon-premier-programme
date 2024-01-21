a63b94badb4f1cc9980fba0431479a64ed9be4d6
nyamarinette <nyamarinette2020@gmail.com>
2024-01-21 08:00:03 +0200

    first commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Hello and welcome!");
+        System.out.println("\t\t Régalons-nous avec du language Java pour debutants. ");
+
+        System.out.print("Veuillez Attacher ");
+        System.out.print("votre ");
+        System.out.print("ceinture !!! \n ");
+        System.out.print("On va s'envoler dans du code. Merci ");
+
+    }
+}
